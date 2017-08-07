e803708ea45aafe6c917425a29703500f3ed79b5
Jesse Wolgamott <jesse@comalproductions.com>
2017-08-07 10:05:09 -0500

    Initial empty mongoose / express

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express")
+const app = express()
+
+const mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
+const databaseURL = process.env.DATABASE || 'mongodb://localhost:27017/giffy'
+mongoose.connect(databaseURL);
+
+const port = process.env.PORT || 5000
+app.listen( port, () => {
+  console.log(`listening on ${port}`)
+})
