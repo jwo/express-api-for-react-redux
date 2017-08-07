c16b2edcc07768f3eaa93416aab71d85e8ee2949
Jesse Wolgamott <jesse@comalproductions.com>
2017-08-07 10:35:37 -0500

    Add models, and registration/auth routes

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-const express = require("express")
-const app = express()
-
+const express = require("express");
+const app = express();
+const bodyParser = require('body-parser');
+const randToken = require("rand-token")
 const mongoose = require('mongoose');
+const User = require("./models/User")
 mongoose.Promise = require('bluebird');
 const databaseURL = process.env.DATABASE || 'mongodb://localhost:27017/giffy'
 mongoose.connect(databaseURL);
@@ -10,3 +12,74 @@ const port = process.env.PORT || 5000
 app.listen( port, () => {
   console.log(`listening on ${port}`)
 })
+
+app.use(bodyParser.json())
+
+app.post("/api/auth", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  User.findOne({username: username, password: password})
+  .then( user => {
+    if (user){
+      res.json({
+        username: user.username,
+        token: user.token
+      })
+    } else {
+      res.status(422).json({
+        errors: ["There was a problem with your username or password"]
+      })
+    }
+  })
+})
+
+app.post("/api/registration", (req,res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+
+  const user = new User({})
+  user.username = username;
+  user.password = password;
+  user.token = randToken.generate(16);
+  user.save()
+  .then( user => {
+    res.status(201).json({
+      username: user.username,
+      token: user.token
+    })
+  })
+  .catch( (e) => {
+    console.log(e)
+
+    if (e.code === 11000){
+      res.status(422).json({
+        errors: ["The username already exists"]
+      })
+    } else {
+      let errors = []
+      if (e.errors.username) {
+        errors.push("Username is required")
+      }
+      if (e.errors.password){
+        errors.push("There was a problem with your password")
+      }
+      res.status(422).json({
+        errors: errors
+      })
+    }
+
+
+  })
+})
+
+// send in a "Authentication: Bearer $TOKEN"
+// without, you'll get a 401
+// Only endpoints without the 401: registration / auth
+
+// GET https://api.giffy.com/api/me -> "Information about me"
+// GET https://api.giffy.com/api/me/gifs -> "My Gifs"
+// GET https://api.giffy.com/api/gifs -> "All Gifs"
+//
+// POST https://api.giffy.com/api/registration -> "create a user"
+// POST https://api.giffy.com/api/auth -> "give you a token if you give me username/password"
+// POST https://api.giffy.com/api/gifs -> "create a gif"
---
+++ models/Gif.js
@@ -0,0 +1,9 @@
+const mongoose = require('mongoose');
+
+const gifSchema = new mongoose.Schema({
+  userId: { type: ObjectId, required: true },
+  url: { type: String, required: true }
+})
+
+const Gif = mongoose.model('Gif', gifSchema)
+module.exports = Gif
